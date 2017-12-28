26c8b0ae03d0f8fb8a3ba76a5bdc7147a75c8b6d
Łukasz Jędrzejewski <kdmrulez@gmail.com>
2017-12-28 09:27:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header><p className="App-intro" />
+  </div>
+);
+
+export default App;
